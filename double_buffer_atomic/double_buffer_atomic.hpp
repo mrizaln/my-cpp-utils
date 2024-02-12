911e81fabf82aaba62074a5e7f410d841e955254
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-02-12 18:18:08 +0700

    Add template parameter on whether to allocate buffer on the heap

--- double_buffer_atomic/double_buffer_atomic.hpp
+++ double_buffer_atomic/double_buffer_atomic.hpp
@@ -3,16 +3,45 @@
 
 #include <array>
 #include <atomic>
+#include <concepts>
 #include <cstdint>
+#include <memory>
 
-template <typename Buffer>
+// Single-producer, single-consumer double buffer with atomic swap
+template <std::copyable Buffer, bool DynamicAlloc = false>
 class DoubleBufferAtomic
 {
 public:
+    using BufferType  = Buffer;
+    using BuffersType = std::conditional_t<DynamicAlloc, std::unique_ptr<Buffer[]>, std::array<Buffer, 2>>;
+
+    static bool constexpr s_dynamicAlloc = DynamicAlloc;
+
+    enum class BufferIndex : std::uint8_t
+    {
+        First  = 0,
+        Second = 1,
+    };
+
+    enum class BufferUpdateStatus : std::uint16_t
+    {
+        Idle,
+        Updating,
+        Done,
+    };
+
     explicit DoubleBufferAtomic(Buffer startState = {})
+        requires(!DynamicAlloc)
         : m_buffers{ startState, startState }
     {
-        m_index = { .m_front = BufferIndex::First, .m_back = BufferIndex::Second };
+    }
+
+    explicit DoubleBufferAtomic(Buffer startState = {})
+        requires(DynamicAlloc)
+        : m_buffers{ std::make_unique<Buffer[]>(2) }
+    {
+        m_buffers[0] = startState;
+        m_buffers[1] = startState;
     }
 
     const Buffer& swapBuffers()
@@ -41,22 +70,10 @@ public:
         m_info = BufferUpdateStatus::Done;
     }
 
-    const Buffer& getFront() const { return getBuffer(m_index.load().m_front); }
+    const Buffer&      getFront() const { return getBuffer(m_index.load().m_front); }
+    BufferUpdateStatus status() const { return m_info.load(); }
 
 private:
-    enum class BufferIndex : std::uint8_t
-    {
-        First  = 0,
-        Second = 1,
-    };
-
-    enum class BufferUpdateStatus : std::uint16_t
-    {
-        Idle     = 0,
-        Updating = 1,
-        Done     = 2,
-    };
-
     struct BufferIndexPair
     {
         BufferIndex m_front = BufferIndex::First;
@@ -65,12 +82,12 @@ private:
 
     static std::size_t toSize(BufferIndex index) { return static_cast<std::size_t>(index); }
 
-    const Buffer& getBuffer(BufferIndex index) const { return m_buffers[static_cast<std::size_t>(index)]; }
-    Buffer&       getBuffer(BufferIndex index) { return m_buffers[static_cast<std::size_t>(index)]; }
+    const Buffer& getBuffer(BufferIndex index) const { return m_buffers[toSize(index)]; }
+    Buffer&       getBuffer(BufferIndex index) { return m_buffers[toSize(index)]; }
 
+    BuffersType                     m_buffers{};
+    std::atomic<BufferIndexPair>    m_index{};
     std::atomic<BufferUpdateStatus> m_info = BufferUpdateStatus::Idle;
-    std::atomic<BufferIndexPair>    m_index;
-    std::array<Buffer, 2>           m_buffers{};
 };
 
 #endif /* end of include guard: DOUBLE_BUFFER_ATOMIC_HPP_T4PFY34R */
--- double_buffer_atomic/double_buffer_atomic_test.cpp
+++ double_buffer_atomic/double_buffer_atomic_test.cpp
@@ -11,10 +11,8 @@ using Buffer = std::string;
 
 std::atomic<bool> g_interrupt{ false };
 
-void interruptHnadler(int /* sig */)
+void interruptHandler(int /* sig */)
 {
-    // trigger tsan "signal-unsafe call inside of signal" possibly because this function may allocate memory
-    // println("Interrupt signal received. Exiting...");
     std::puts("Interrupt signal received. Exiting...");
     g_interrupt.store(true);
     g_interrupt.notify_all();
@@ -22,20 +20,31 @@ void interruptHnadler(int /* sig */)
 
 int main()
 {
-    std::signal(SIGINT, interruptHnadler);
+    std::signal(SIGINT, interruptHandler);
 
-    // println("hello world");
-    DoubleBufferAtomic<Buffer> db{ "start" };
+    using DoubleBuffer = DoubleBufferAtomic<Buffer, false>;    // on the stack (using std::array)
+    // using DoubleBuffer = DoubleBufferAtomic<Buffer, true>;     // dynamic memory allocation (on the heap)
+
+    DoubleBuffer db{ "start" };
+
+    println("sizeof DoubleBufferAtomic<Buffer>= {}", sizeof(DoubleBuffer));
+    println("sizeof Buffer [array] (dy: {:<5})= {}", DoubleBuffer::s_dynamicAlloc, sizeof(DoubleBuffer::BuffersType));
+    println("sizeof Buffer                    = {}", sizeof(DoubleBuffer::BufferType));
+    println("sizeof BufferUpdateStatus        = {}", sizeof(DoubleBuffer::BufferUpdateStatus));
+    println("sizeof BufferIndex               = {}", sizeof(DoubleBuffer::BufferIndex));
 
     // producer thread
-    std::jthread t1([&db](std::stop_token st) {
+    std::jthread t1([&db](const std::stop_token& st) {
         int counter = 0;
         while (!st.stop_requested()) {
+            /* pretend to do some work */
+
             db.updateBuffer([&counter](Buffer& buffer) {
                 buffer = std::format("{0} ==> {0:032b}", counter);
-                println("t1: updated buffer: {}", buffer);
+                println("t1: [U] buffer: {}", buffer);
                 std::this_thread::sleep_for(239ms);
             });
+
             ++counter;
             println("t1: counter: {}", counter);
             std::this_thread::sleep_for(134ms);
@@ -43,13 +52,38 @@ int main()
     });
 
     // consumer thread
-    std::jthread t2([&db](std::stop_token st) {
+    std::jthread t2([&db](const std::stop_token& st) {
         while (!st.stop_requested()) {
+            /* pretend to do some work */
+
             const auto& buffer = db.swapBuffers();
-            println("t2: swapped buffer: {}", buffer);
-            std::this_thread::sleep_for(1008ms);
+
+            println("t2: (S) buffer: {}", buffer);
+            std::this_thread::sleep_for(1078ms);
         }
     });
+
+    // // second consumer thread (unsynchronized access to the buffer)
+    // std::jthread t3([&db](const std::stop_token& st) {
+    //     while (!st.stop_requested()) {
+    //         /* pretend to do some work */
+    //
+    //         const auto& buffer = db.getFront();
+    //
+    //         println("t3: (F) buffer: {}", buffer); // unsynchronized access: the buffer might ber swapped while read
+    //         std::this_thread::sleep_for(108ms);
+    //     }
+    // });
+
+    // multiple consumer might be possible with sub-consumer like following:
+    // - consumer: swaps the buffer
+    // - consumer: spawns (sync or async) sub-consumers
+    //      - sub-consumer 1: reads the buffer
+    //      - sub-consumer 2: reads the buffer
+    //      - sub-consumer 3: reads the buffer
+    //      - ...
+    // - consumer: waits for all sub-consumers to finish (possibly with a barrier)
+    // - consumer: repeats
 
     g_interrupt.wait(false);
 
